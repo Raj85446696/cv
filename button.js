8a2458d4a2e2ffd81a70f39734f44cf1cc2f1038
RITU RAJ KUMAR <146192378+Raj85446696@users.noreply.github.com>
2024-07-28 15:25:22 +0530

    Add files via upload

---
+++ button.js
@@ -0,0 +1,7 @@
+document.getElementById("Downloadnow").addEventListener("click",function(){
+    // url of the website
+    const newUrl = "https://drive.google.com/drive/folders/14MtrAKVjSlfBD4_peiVVR72kxIVn7J8r";
+
+    //redirect to new url 
+    window.location.href = newUrl;
+});
